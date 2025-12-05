f81960e466583369f203ed41d214dc855ec0a468
IZT612 <ivan.zamora@iesnervion.es>
2025-12-05 10:16:27 +0100

    update

--- Tema1/Proyectos/api_personas/main.py
+++ Tema1/Proyectos/api_personas/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import moviles, personas, auth_users
+from Tema1.Proyectos.api_personas.routers.db.models import personasModel
 
 app = FastAPI()
 
@@ -7,3 +8,4 @@ app = FastAPI()
 app.include_router(moviles.router)
 app.include_router(personas.router)
 app.include_router(auth_users.router)
+app.include_router(personasModel.router)
